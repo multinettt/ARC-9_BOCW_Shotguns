50b6e7fb0f8b57663364680d6e0ec9cdb5219c33
multinett <72457258+multinettt@users.noreply.github.com>
2025-01-24 23:11:19 +0300

    Gallo SA12

--- lua/arc9/shared/sh_arc9_bocw_shotguns_sounds.lua
+++ lua/arc9/shared/sh_arc9_bocw_shotguns_sounds.lua
@@ -88,3 +88,110 @@ sound.Add({
     volume = 1.0,
     sound = "weapons/arc9/bocw_hauer77/hauer77_fire_silenced.wav"
 })
+
+--========== Gallo SA12 =======================================================
+-- SHELLS
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_shellinstart",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = { "weapons/arc9/bocw_gallosa12/gallosa12_reload_shellinstart1.wav", "weapons/arc9/bocw_gallosa12/gallosa12_reload_shellinstart2.wav", "weapons/arc9/bocw_gallosa12/gallosa12_reload_shellinstart3.wav" }
+})
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_shellin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = { "weapons/arc9/bocw_gallosa12/gallosa12_reload_shellin1.wav", "weapons/arc9/bocw_gallosa12/gallosa12_reload_shellin2.wav", "weapons/arc9/bocw_gallosa12/gallosa12_reload_shellin3.wav", "weapons/arc9/bocw_gallosa12/gallosa12_reload_shellin4.wav", "weapons/arc9/bocw_gallosa12/gallosa12_reload_shellin5.wav" }
+})
+
+-- READY
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_ready_start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_ready_start.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_ready_boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_ready_boltback.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_ready_boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_ready_boltrelease.wav"
+})
+
+-- RELOAD FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_reload_start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = { "weapons/arc9/bocw_gallosa12/gallosa12_reload_start1.wav", "weapons/arc9/bocw_gallosa12/gallosa12_reload_start2.wav" }
+})
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_reload_end",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = { "weapons/arc9/bocw_gallosa12/gallosa12_reload_end1.wav", "weapons/arc9/bocw_gallosa12/gallosa12_reload_end2.wav" }
+})
+
+-- INSPECT FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_inspect",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_inspect.wav"
+})
+
+-- PULLOUT PUTAWAY
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_draw",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_draw.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_holster",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_holster.wav"
+})
+
+-----------------------------------------------
+-- FIRING SOUNDS
+
+sound.Add({ -- BLACK OPS COLD WAR
+    name = "ARC9_BOCW.GalloSA12_fire",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_fire.wav"
+})
+
+sound.Add({ -- WARZONE
+    name = "ARC9_BOCW.GalloSA12_fire_wz",
+    channel = CHAN_STATIC,
+    volume = 0.7,
+    level = 100,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_fire_warzone.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.GalloSA12_fire_silenced",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_gallosa12/gallosa12_fire_silenced.wav"
+})
